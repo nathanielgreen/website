34d0482ff946a9d606a1c696aa349f387f30cd37
Nathaniel Green <ngreen2303@gmail.com>
2016-10-26 21:13:46 +0100

    Updating theme and adding new images

--- scripts.js
+++ scripts.js
@@ -1,39 +1,5 @@
 console.log("scripts working");
 
-$(document).ready(function() {
-    
-  $(window).scroll( function(){
-    
-    $('.fadein').each( function(i){
-        
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > bottom_of_object ){
-            
-            $(this).animate({'opacity':'1'},500);
-                
-        }
-        
-    });
-
-    $('.project-title').each( function(i){
-        
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > bottom_of_object ){
-            
-            $(this).animate({'opacity':'1'},500);
-                
-        }
-        
-    });
-    
-  });
-    
-});
-
-
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  
+};
