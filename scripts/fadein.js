c6db2d3778d76e2bf0700b6aad33abe4b15e325b
Nathaniel Green <ngreen2303@gmail.com>
2016-10-29 13:45:23 +0100

    Adding project information in progress

--- scripts/fadein.js
+++ scripts/fadein.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         
         /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > bottom_of_object/1.2 ){
+        if( bottom_of_window > bottom_of_object/1.15 ){
             
             $(this).animate({'opacity':'1'},500);
                 
